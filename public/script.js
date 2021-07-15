7c64e3367fdb0427656b43aed62a7d1b8afcff7d
broneukr <69394343+broneukr@users.noreply.github.com>
2021-07-15 15:22:49 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -20,7 +20,7 @@ document.querySelector('button[type="submit"]').addEventListener('click', (e) =>
     const options = {
         method: "POST",
         headers: { "Content-Type": "application/json; charset=utf-8" },
-        body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
+        body: JSON.stringify({ url: pageToScreenshot })
     };
 
     document.getElementById('result').textContent = "Please wait..."
